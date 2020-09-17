b9f512b2d436003725da6464a31909f0e14f4dfd
Dadinz <ahmadinations@gmail.com>
2020-09-17 13:07:34 +0700

    Setup Middleware Admin & Staff, Laravel ui auth, Relasi model user & role

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function(){
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
+
+Route::group(['as' => 'staff.', 'prefix' => 'staff', 'namespace' => 'Staff', 'middleware' => ['auth', 'staff']], function(){
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
